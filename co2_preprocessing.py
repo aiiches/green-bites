d64bbc39d100352ea7322f73ec444e12c1bc20b1
kateszabo <kate_szabo@rogers.com>
2021-06-09 09:39:37 -0400

    Changes '(F)' to 'FROZEN' and fixes some spelling mistakes in labels

--- co2_preprocessing.py
+++ co2_preprocessing.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import os
+import re
 
 # load food footprints data from excel
 cwd = os.getcwd()
@@ -13,16 +14,21 @@ df = df.drop(['Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23'], axis=
 
 # clean labels (remove tags)
 df['Item'] = df['Item'].str.replace(r'\*', '')
+
+# Replace 'FOOD (F)' with 'FROZEN FOOD'
+# for index, row in df.iterrows():
+#     if re.match(r".+ \(F\)", row['Item']):  # if the item is in format "FOOD (F)"
+#         food_name = row['Item'].replace(r'\(F\)', '')
+#         df.loc[index].at["Item"] = f'FROZEN {food_name}'
+
+df['Item'] = df['Item'].str.replace(r'\(F\)', 'FROZEN')
 df['Item'] = df['Item'].str.replace(r'\(.\)', '')
 df['Item'] = df['Item'].str.strip()
 
-print(df)
-
+# create simplified dataframe
 df2 = [df['Item'], df['mean']]
 headers = ['Item', 'footprint']
 df2 = pd.concat(df2, axis=1, keys=headers)
-
-print(df2)
 
 # write to csv
 df.to_csv('co2_data.csv')

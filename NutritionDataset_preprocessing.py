fbcf29de60243f280dc83c2d5f52507b26ee57f1
mekah15 <50745258+mekah15@users.noreply.github.com>
2021-06-09 16:05:04 -0400

    New nutritionnal dataset + cleaning

--- NutritionDataset_preprocessing.py
+++ NutritionDataset_preprocessing.py
@@ -4,27 +4,23 @@ import os
 
 # load food footprints data from excel
 cwd = os.getcwd()
-data_path = os.path.join(cwd, 'data\\Food_Nutrients.xlsx')
+data_path = os.path.join(cwd, 'data\\nutritionnalvalues.xlsx')
 df = pd.read_excel(data_path)
 
-# Change abreviations
-
-df['Shrt_Desc'] = df['Shrt_Desc'].str.replace('ALCOHOLIC BEV,', '')
-df['Shrt_Desc'] = df['Shrt_Desc'].str.replace('TABLE,', '')
+# Remove non descriptive categories
+df['Descrip'] = df['Descrip'].str.replace('Alcoholic beverage,', '')
+df['Descrip'] = df['Descrip'].str.replace('table,', '')
 
 # separate first row
-df[['Type', 'Name', 'Others']] = df.Shrt_Desc.str.split(",", n=2, expand=True)
+df[['Type', 'Name', 'Others']] = df.Descrip.str.split(",", n=2, expand=True)
 
 # drop unwanted columns
-df = df.drop(['Shrt_Desc', 'Others', 'NDB_No', 'GmWt_Desc1', 'GmWt_Desc2'], axis=1)
-# should we remove others colomns (Those with too many missing values)
-
-# If the cell is empty, replace by 0 (Don't know if its the best way to do it)
-df.fillna(0, inplace=True)
+df = df.drop(['ShortDescrip', 'Descrip', 'Others', 'ID', 'MfgName', 'ScientificName'], axis=1)
 
 # averaging similar rows
-df2 = df.groupby(['Type', 'Name'], as_index=False).mean()
+df2 = df.groupby(['FoodGroup','Type', 'Name'], as_index=False).mean()
 
 
 # write to csv
+
 df2.to_csv('Nutrients_data.csv')

84c5dd61d907ed75b25828483e153f8d122d8968
Kate Szabo <55710939+kateszabo@users.noreply.github.com>
2021-06-11 10:19:14 -0400

    Find nutrient words that are excluded from the recipe dataset (#19)
    * Find nutrient words that are excluded from the recipe dataset
    
    * Fix typo
    
    Co-authored-by: kateszabo <kate_szabo@rogers.com>

--- corpus_preprocessing.py
+++ corpus_preprocessing.py
@@ -5,38 +5,61 @@ import os
 from itertools import chain
 import re
 
+pattern = re.compile('[\W_0-9]+')
+
 # load data from excel files
 cwd = os.getcwd()
 co2_path = os.path.join(cwd, 'co2_data_simple.csv')
+nutrients_path = os.path.join(cwd, 'Nutrients_data.csv')
 recipes_path = os.path.join(cwd, 'data\\recipes.xls')
 
 # crate dataframes
 co2_df = pd.read_csv(co2_path)
+nutrient_df = pd.read_csv(nutrients_path)
 recipes_df = pd.read_excel(recipes_path)
 
 # format co2 word data
 co2_words = co2_df['Item'].to_list()
 for i, item in enumerate(co2_words):
     item = item.lower()
-    co2_words[i] = item.split()
+    item = item.split()
+    for j, word in enumerate(item):
+        item[j] = pattern.sub(' ', word)
+    co2_words[i] = item
 co2_words = list(chain.from_iterable(co2_words))
 co2_words = set(co2_words)
 
+# format nutrient word data (uses type and name)
+nutrient_words = nutrient_df['Type'].to_list() + nutrient_df['Name'].to_list()
+for i, item in enumerate(nutrient_words):
+    item = item.lower()
+    item = item.split()
+    for j, word in enumerate(item):
+        item[j] = pattern.sub(' ', word)
+    nutrient_words[i] = item
+nutrient_words = list(chain.from_iterable(nutrient_words))
+nutrient_words = set(nutrient_words)
+
 # format recipe word data
 recipe_words = recipes_df['Recipes'].to_list()
-pattern = re.compile('[\W_0-9]+')
 for i, item in enumerate(recipe_words):
     item = item.split()
     for j, word in enumerate(item):
         word = word.lower()
-        word = pattern.sub('', word)
+        word = pattern.sub(' ', word)
         item[j] = word
     recipe_words[i] = item
 recipe_words = list(chain.from_iterable(recipe_words))
 recipe_words = set(recipe_words)
 
-# find overlap
-overlap = co2_words.intersection(recipe_words)
-difference = co2_words.difference(recipe_words)
-percent_excluded = int((len(difference) / len(co2_words)) * 100)
-print(f'{len(difference)} words from the co2 dataset (about {percent_excluded}%) are not included in the recipes')
+# find excluded words
+co2_difference = co2_words.difference(recipe_words)
+co2_percent = int((len(co2_difference) / len(co2_words)) * 100)
+nutrients_difference = nutrient_words.difference(recipe_words)
+nutrients_percent = int((len(nutrients_difference) / len(nutrient_words)) * 100)
+
+print(f'Excluded co2 words: {co2_difference}')
+print(f'{len(co2_difference)} words from the co2 dataset (about {co2_percent}%) are not included in the recipes')
+
+print(f'Excluded nutrient words: {nutrients_difference}')
+print(f'{len(nutrients_difference)} words from the nutrient dataset (about {nutrients_percent}%) are not included in the recipes')

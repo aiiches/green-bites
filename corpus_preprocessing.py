78218149cd70e417d41a73f8fa0d81f1322ee629
Kate Szabo <55710939+kateszabo@users.noreply.github.com>
2021-06-11 16:06:58 -0400

    Filter data by overlapping words (#20)
    * Add co2 and nutrient data filtered by known words
    
    * Re-organize pre-processing
    
    * Remove extra data files
    
    * Re-name files
    
    * format
    
    Co-authored-by: kateszabo <kate_szabo@rogers.com>

--- corpus_preprocessing.py
+++ corpus_preprocessing.py
@@ -1,56 +1,36 @@
-
-import numpy as np
 import pandas as pd
 import os
 from itertools import chain
-import re
-
-pattern = re.compile('[\W_0-9]+')
 
 # load data from excel files
 cwd = os.getcwd()
-co2_path = os.path.join(cwd, 'co2_data_simple.csv')
-nutrients_path = os.path.join(cwd, 'Nutrients_data.csv')
-recipes_path = os.path.join(cwd, 'data\\recipes.xls')
-
-# crate dataframes
-co2_df = pd.read_csv(co2_path)
-nutrient_df = pd.read_csv(nutrients_path)
-recipes_df = pd.read_excel(recipes_path)
+co2_df = pd.read_csv(os.path.join(cwd, 'co2_data.csv'))
+nutrient_df = pd.read_csv(os.path.join(cwd, 'nutrition_data.csv'))
+recipes_df = pd.read_csv(os.path.join(cwd, 'recipe_data.csv'))
 
 # format co2 word data
 co2_words = co2_df['Item'].to_list()
 for i, item in enumerate(co2_words):
-    item = item.lower()
-    item = item.split()
-    for j, word in enumerate(item):
-        item[j] = pattern.sub(' ', word)
-    co2_words[i] = item
+    co2_words[i] = item.split()
 co2_words = list(chain.from_iterable(co2_words))
 co2_words = set(co2_words)
+print(co2_words)
 
 # format nutrient word data (uses type and name)
-nutrient_words = nutrient_df['Type'].to_list() + nutrient_df['Name'].to_list()
+nutrient_words = nutrient_df['FullName'].to_list()
 for i, item in enumerate(nutrient_words):
-    item = item.lower()
-    item = item.split()
-    for j, word in enumerate(item):
-        item[j] = pattern.sub(' ', word)
-    nutrient_words[i] = item
+    nutrient_words[i] = item.split()
 nutrient_words = list(chain.from_iterable(nutrient_words))
 nutrient_words = set(nutrient_words)
+print(nutrient_words)
 
 # format recipe word data
 recipe_words = recipes_df['Recipes'].to_list()
 for i, item in enumerate(recipe_words):
-    item = item.split()
-    for j, word in enumerate(item):
-        word = word.lower()
-        word = pattern.sub(' ', word)
-        item[j] = word
-    recipe_words[i] = item
+    recipe_words[i] = item.split()
 recipe_words = list(chain.from_iterable(recipe_words))
 recipe_words = set(recipe_words)
+print(recipe_words)
 
 # find excluded words
 co2_difference = co2_words.difference(recipe_words)
@@ -62,4 +42,25 @@ print(f'Excluded co2 words: {co2_difference}')
 print(f'{len(co2_difference)} words from the co2 dataset (about {co2_percent}%) are not included in the recipes')
 
 print(f'Excluded nutrient words: {nutrients_difference}')
-print(f'{len(nutrients_difference)} words from the nutrient dataset (about {nutrients_percent}%) are not included in the recipes')
+print(f'{len(nutrients_difference)} words from the nutrient dataset (about {nutrients_percent}%) '
+      f'are not included in the recipes')
+
+# Filter co2 data
+rows_to_drop = []
+for i, name in enumerate(co2_df['Item'].to_list()):
+    for word in co2_difference:
+        if name.find(word) != -1:  # name contains a word from co2_difference
+            rows_to_drop.append(i)
+            break
+co2_df = co2_df.drop(rows_to_drop)
+co2_df.to_csv('co2_data_filtered.csv', index=False)
+
+# filter nutrient data
+rows_to_drop = []
+for i, name in enumerate(nutrient_df['FullName'].to_list()):
+    for word in nutrients_difference:
+        if name.find(word) != -1:  # name contains a word from co2_difference
+            rows_to_drop.append(i)
+            break
+nutrient_df = nutrient_df.drop(rows_to_drop)
+nutrient_df.to_csv('nutrient_data_filtered.csv', index=False)
---
+++ nutrition_preprocessing.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import os
+
+# load food footprints data from excel
+cwd = os.getcwd()
+data_path = os.path.join(cwd, 'data\\nutrition_data_raw.xlsx')
+df = pd.read_excel(data_path)
+
+# Remove non descriptive categories
+df['Descrip'] = df['Descrip'].str.replace('Alcoholic beverage,', '')
+df['Descrip'] = df['Descrip'].str.replace('table,', '')
+
+# separate first row
+df[['Type', 'Name', 'Others']] = df.Descrip.str.split(",", n=2, expand=True)
+
+# drop unwanted columns
+df = df.drop(['ShortDescrip', 'Descrip', 'Others', 'ID', 'MfgName', 'ScientificName', 'VitA_USRDA', 'VitB6_USRDA',
+              'VitB12_USRDA', 'VitC_USRDA', 'VitE_USRDA', 'Folate_USRDA', 'Niacin_USRDA', 'Riboflavin_USRDA',
+              'Thiamin_USRDA', 'Calcium_USRDA', 'Copper_USRDA', 'Magnesium_USRDA', 'Phosphorus_USRDA',
+              'Selenium_USRDA', 'Zinc_USRDA'], axis=1)
+
+# averaging similar rows
+df = df.groupby(['FoodGroup', 'Type', 'Name'], as_index=False).mean()
+
+# Get "Full Name" and format
+df.insert(0, 'FullName', df['Type'] + df['Name'])
+df['FullName'] = df['FullName'].str.lower()
+df['FullName'] = df['FullName'].str.replace('(', '')
+df['FullName'] = df['FullName'].str.replace(')', '')
+df['FullName'] = df['FullName'].str.replace('&', 'and')
+df['FullName'] = df['FullName'].str.replace('/', ' ')
+df['FullName'] = df['FullName'].str.replace('-', ' ')
+df['FullName'] = df['FullName'].str.strip()
+
+# Drop more unwanted columns
+df = df.drop(['FoodGroup', 'Type', 'Name'], axis=1)
+
+# write to csv
+df.to_csv('nutrition_data.csv', index=False)

93ce680a93d1e9b3a6d70b4d813cafb5c614d577
kateszabo <kate_szabo@rogers.com>
2021-06-15 15:39:16 -0400

    First pass at merging the datasets

--- merge_datasets_vectors.py
+++ merge_datasets_vectors.py
@@ -1,48 +1,54 @@
 import numpy as np
 import pandas as pd
 import os
+from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import cosine_similarity
 
-# load trained word2vec model
-
+# load datasets
 cwd = os.getcwd()
-emb_df = pd.read_csv(os.path.join(cwd, 'embeddings.csv'), index_col=0)
 co2_df = pd.read_csv(os.path.join(cwd, 'co2_data_filtered.csv'))
 nutrient_df = pd.read_csv(os.path.join(cwd, 'nutrition_data_filtered.csv'))
 
-print(emb_df)
+# load trained word2vec model
+wv = KeyedVectors.load("word2vec.wordvectors", mmap='r')
+
 # Get name embeddings for each label in the nutrition dataset, and include in the dataframe
 embeddings_list = []
-vector_sum = np.zeros(100)
-item_name = nutrient_df['FullName'].to_list()[0]
-for word in item_name.split():  # for each word in the item name
-    vector = emb_df.loc[word]  # find the vector from the embeddings dataset
-    vector = vector.to_numpy()
-    print(f'{word}: {vector}')
-    vector_sum = np.add(vector_sum, vector)
-print(vector_sum)
+for index, row in nutrient_df.iterrows():
+    vector_sum = np.zeros(100)
+    item_name = row['FullName']
+    for i, word in enumerate(item_name.split()):  # for each word in the item name
+        vector = wv[word]  # find the vector from the embeddings dataset
+        vector_sum = np.add(vector_sum, vector)
+    embeddings_list.append(vector_sum / (i + 1))
+nutrient_df.insert(1, 'WordEmbedding', embeddings_list)
 
-# For each name in the nutrition dataset, get word embeddings for each word
+# For each name in the co2 dataset, get word embeddings for each word
+embeddings_list = []
+for index, row in co2_df.iterrows():
+    vector_sum = np.zeros(100)
+    item_name = row['Item']
+    for i, word in enumerate(item_name.split()):  # for each word in the item name
+        vector = wv[word]  # find the vector from the embeddings dataset
+        vector_sum = np.add(vector_sum, vector)
+    embeddings_list.append(vector_sum / (i + 1))
+co2_df.insert(1, 'WordEmbedding', embeddings_list)
 
+# Find the best nutrition matches between the two datasets
+vector1 = co2_df['WordEmbedding'].tolist()
+vector1 = np.array(vector1)
 
-# Add the vectors together to get the embedding for the full name
+vector2 = nutrient_df['WordEmbedding'].tolist()
+vector2 = np.array(vector2)
 
+similarities = cosine_similarity(vector1, vector2)
+max_1 = np.argmax(similarities, axis=0)  # for each element in the nutrition dataset
+max_2 = np.argmax(similarities, axis=1)  # for each element in the co2 dataset
 
-# Add this to the dataframe
+df_tomerge = pd.DataFrame(columns=(nutrient_df.columns))
+for i, index in enumerate(max_2):
+    df_tomerge.loc[i] = nutrient_df.loc[index]
 
-
-# Find the best nutrition match for each word in the co2 dataset
-
-
-# For a single co2 entry, find word embeddings for each word in the co2 dataset
-
-
-# Add the vectors together to get the embedding for the full name
-
-
-# Calculate the cosine similarity with each element of the nutrition dataset
-
-
-# Pick the nutrition element that has the largest similarity
-
-
-# Append the nutrition data to the co2 entry
+df_merged = pd.concat([co2_df, df_tomerge], axis=1)
+df_merged = df_merged.drop(['WordEmbedding'], axis=1)
+df_merged.to_csv('merged_dataset.csv', index=False)

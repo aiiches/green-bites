b6a007c31658eb3f82efe7e701d20f236bc4f536
Kate Szabo <55710939+kateszabo@users.noreply.github.com>
2021-06-16 09:06:20 -0400

    Ks/merge data (#25)
    * Organize word_vectors.csv (not included in commit because of large file size)
    
    * re-filter datasets by actual known words from word2vec
    
    * Update pre-processing
    
    * First pass at merging the datasets
    
    * Create larger merged dataset
    
    * Add item recognition code
    
    * Small change to UI
    
    Co-authored-by: kateszabo <kate_szabo@rogers.com>

--- corpus_preprocessing.py
+++ corpus_preprocessing.py
@@ -6,7 +6,7 @@ from itertools import chain
 cwd = os.getcwd()
 co2_df = pd.read_csv(os.path.join(cwd, 'co2_data.csv'))
 nutrient_df = pd.read_csv(os.path.join(cwd, 'nutrition_data.csv'))
-recipes_df = pd.read_csv(os.path.join(cwd, 'recipe_data.csv'))
+recipes_df = pd.read_csv(os.path.join(cwd, 'embeddings.csv'), index_col=0)
 
 # format co2 word data
 co2_words = co2_df['Item'].to_list()
@@ -14,7 +14,7 @@ for i, item in enumerate(co2_words):
     co2_words[i] = item.split()
 co2_words = list(chain.from_iterable(co2_words))
 co2_words = set(co2_words)
-print(co2_words)
+# print(co2_words)
 
 # format nutrient word data (uses type and name)
 nutrient_words = nutrient_df['FullName'].to_list()
@@ -22,15 +22,15 @@ for i, item in enumerate(nutrient_words):
     nutrient_words[i] = item.split()
 nutrient_words = list(chain.from_iterable(nutrient_words))
 nutrient_words = set(nutrient_words)
-print(nutrient_words)
+# print(nutrient_words)
 
 # format recipe word data
-recipe_words = recipes_df['Recipes'].to_list()
-for i, item in enumerate(recipe_words):
-    recipe_words[i] = item.split()
-recipe_words = list(chain.from_iterable(recipe_words))
+recipe_words = recipes_df.index.values
+# for i, item in enumerate(recipe_words):
+#     recipe_words[i] = item.split()
+# recipe_words = list(chain.from_iterable(recipe_words))
 recipe_words = set(recipe_words)
-print(recipe_words)
+# print(recipe_words)
 
 # find excluded words
 co2_difference = co2_words.difference(recipe_words)
@@ -63,4 +63,4 @@ for i, name in enumerate(nutrient_df['FullName'].to_list()):
             rows_to_drop.append(i)
             break
 nutrient_df = nutrient_df.drop(rows_to_drop)
-nutrient_df.to_csv('nutrient_data_filtered.csv', index=False)
+nutrient_df.to_csv('nutrition_data_filtered.csv', index=False)
---
+++ merge_datasets_vectors.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import os
+from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import cosine_similarity
+
+# load datasets
+cwd = os.getcwd()
+co2_df = pd.read_csv(os.path.join(cwd, 'co2_data_filtered.csv'))
+nutrient_df = pd.read_csv(os.path.join(cwd, 'nutrition_data_filtered.csv'))
+
+# load trained word2vec model
+wv = KeyedVectors.load("word2vec.wordvectors", mmap='r')
+
+# Get name embeddings for each label in the nutrition dataset, and include in the dataframe
+embeddings_list = []
+for index, row in nutrient_df.iterrows():
+    vector_sum = np.zeros(100)
+    item_name = row['FullName']
+    for i, word in enumerate(item_name.split()):  # for each word in the item name
+        vector = wv[word]  # find the vector from the embeddings dataset
+        vector_sum = np.add(vector_sum, vector)
+    embeddings_list.append(vector_sum / (i + 1))
+nutrient_df.insert(1, 'Word Embedding', embeddings_list)
+
+# For each name in the co2 dataset, get word embeddings for each word
+embeddings_list = []
+for index, row in co2_df.iterrows():
+    vector_sum = np.zeros(100)
+    item_name = row['Item']
+    for i, word in enumerate(item_name.split()):  # for each word in the item name
+        vector = wv[word]  # find the vector from the embeddings dataset
+        vector_sum = np.add(vector_sum, vector)
+    embeddings_list.append(vector_sum / (i + 1))
+co2_df.insert(1, 'WordEmbedding', embeddings_list)
+
+# Find the best nutrition matches between the two datasets
+vector1 = co2_df['WordEmbedding'].tolist()
+vector1 = np.array(vector1)
+
+vector2 = nutrient_df['Word Embedding'].tolist()
+vector2 = np.array(vector2)
+
+similarities = cosine_similarity(vector1, vector2)
+max_1 = np.argmax(similarities, axis=0)  # for each element in the nutrition dataset
+max_2 = np.argmax(similarities, axis=1)  # for each element in the co2 dataset
+
+df_tomerge = pd.DataFrame(columns=(nutrient_df.columns))
+for i, index in enumerate(max_2):
+    df_tomerge.loc[i] = nutrient_df.loc[index]
+
+df_merged = pd.concat([co2_df, df_tomerge], axis=1)
+df_merged = df_merged.drop(['WordEmbedding'], axis=1)
+df_merged.to_csv('merged_dataset.csv', index=False)
+
+df_tomerge2 = pd.DataFrame(columns=(co2_df.columns))
+for i, index in enumerate(max_1):
+    df_tomerge2.loc[i] = co2_df.loc[index]
+
+df_merged = pd.concat([nutrient_df, df_tomerge2], axis=1)
+df_merged = df_merged.drop(['WordEmbedding'], axis=1)
+df_merged.to_csv('merged_dataset_large.csv', index=False)

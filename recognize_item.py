39c96a89659f80286253f2ea79016db827b0fe25
ayesha-cs <75084035+ayesha-cs@users.noreply.github.com>
2021-06-17 15:14:51 -0400

    user_interface_gui

--- recognize_item.py
+++ recognize_item.py
@@ -5,7 +5,7 @@ from gensim.models import KeyedVectors
 from sklearn.metrics.pairwise import cosine_similarity
 
 cwd = os.getcwd()
-df = pd.read_csv(os.path.join(cwd, 'data\\merged_dataset_large.csv'))
+df = pd.read_csv(os.path.join(cwd, 'data/merged_dataset_large.csv'))
 known_embeddings = df['Word Embedding'].apply(lambda x:
                            np.fromstring(
                                x.replace('\n','')
@@ -14,9 +14,9 @@ known_embeddings = df['Word Embedding'].apply(lambda x:
                                 .replace('  ',' '), sep=' '))
 known_embeddings = known_embeddings.tolist()
 known_embeddings = np.array(known_embeddings)
-print(known_embeddings)
+#print(known_embeddings)
 
-model_path = os.path.join(cwd, 'saved_models\\word2vec.wordvectors')
+model_path = os.path.join(cwd, 'saved_models/word2vec.wordvectors')
 wv = KeyedVectors.load(model_path, mmap='r')
 
 
@@ -29,7 +29,7 @@ def recognize_item(item_name, known_embeddings):
     max_index = np.argmax(similarities)
     return max_index
 
-
+'''
 print("Welcome to Green Bites!")
 print("Enter 'Q' to exit")
 user_item = input("Enter any grocery item: ")
@@ -42,4 +42,5 @@ while user_item != "Q":
     finally:
         user_item = input("Enter another item: ")
 
-print("Happy shopping!")
+print("Happy shopping!") 
+'''

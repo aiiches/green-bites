1cd9e61f196cd0d101a65aeb914b8b2c436fd9be
Kate Szabo <55710939+kateszabo@users.noreply.github.com>
2021-06-17 13:08:15 -0400

    Include food identification in console_UI (#35)
    * include food identification in console_UI
    
    * Clean up function calls and print statements
    
    Co-authored-by: kateszabo <kate_szabo@rogers.com>

--- recognize_item.py
+++ recognize_item.py
@@ -14,32 +14,38 @@ known_embeddings = df['Word Embedding'].apply(lambda x:
                                 .replace('  ',' '), sep=' '))
 known_embeddings = known_embeddings.tolist()
 known_embeddings = np.array(known_embeddings)
-print(known_embeddings)
 
 model_path = os.path.join(cwd, 'saved_models\\word2vec.wordvectors')
 wv = KeyedVectors.load(model_path, mmap='r')
 
 
-def recognize_item(item_name, known_embeddings):
+def recognize_item(item_name, emb=known_embeddings):
+    """
+          recognizes the user input and finds the closest match in the full dataset
+          :param emb: list of embeddings in the full dataset (implicit from outer scope)
+          :param item_name: item entered by the user (string)
+          :return: dataframe row containing the item that is the best match from the dataset
+    """
     embedding = np.zeros(100)
     for i, word in enumerate(item_name.split()):
         embedding = np.add(embedding, wv[word])
     embedding = embedding / (i + 1)
-    similarities = cosine_similarity([embedding], known_embeddings)
+    similarities = cosine_similarity([embedding], emb)
     max_index = np.argmax(similarities)
-    return max_index
+    return df.loc[max_index]
 
 
-print("Welcome to Green Bites!")
-print("Enter 'Q' to exit")
-user_item = input("Enter any grocery item: ")
-while user_item != "Q":
-    try:
-        index = recognize_item(user_item, known_embeddings)
-        print(f"{user_item} is estimated to have a footprint of about {df.loc[index]['Footprint']} kg of CO2 per kg.")
-    except:
-        print("Sorry, we couldn't recognize that food item. Try another!")
-    finally:
-        user_item = input("Enter another item: ")
+if __name__ == '__main__':
+    print("Welcome to Green Bites!")
+    print("Enter 'Q' to exit")
+    user_item = input("Enter any grocery item: ")
+    while user_item != "Q":
+        try:
+            item = recognize_item(user_item)
+            print(f"{user_item} is estimated to have a footprint of about {item['Footprint']} kg of CO2 per kg.")
+        except:
+            print("Sorry, we couldn't recognize that food item. Try another!")
+        finally:
+            user_item = input("Enter another item: ")
 
-print("Happy shopping!")
+    print("Happy shopping!")

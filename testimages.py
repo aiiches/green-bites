31104016cf78732ece08341b99db0b7b7b86c155
mekah15 <50745258+mekah15@users.noreply.github.com>
2021-06-18 16:55:25 -0400

    predicting milk

--- testimages.py
+++ testimages.py
@@ -6,27 +6,31 @@ from PIL import Image
 
 cwd = os.getcwd()
 img_path = os.path.join(cwd, 'data/GroceryStoreDataset-master/dataset/')
-img_path2 = img_path + '/test/Fruit/Melon/Watermelon/Watermelon_044.jpg'
+#img_path2 = img_path + '/test/Fruit/Melon/Watermelon/Watermelon_044.jpg'
+img_path2 = img_path + '/test/Packages/Milk/Arla-Lactose-Medium-Fat-Milk/Arla-Lactose-Medium-Fat-Milk_001.jpg'
 image = Image.open(img_path2)
 image = transforms.ToTensor()(image).unsqueeze(0)
 image = image[:,:,0:348,0:348]
 
 df = pd.read_csv(img_path + '/clean_classes.csv')
 
-print(df)
 
-'''
 model_name = "first_model.pt"
 model = torch.load(model_name)
 
 print(image.size())
 
-predictions = model (image)
+predictions = model(image)
 #(1, 43)
 print(predictions.shape)
 #Gives you the class number
-class_number = torch.argmax(predictions)
+class_number = int(torch.argmax(predictions))
+print(class_number)
 #Gives you the class name
 
-Class_name = list_of_class_names[class_number]
-'''
+
+ClassRow = df.loc[df['Coarse Class ID (int)'] == class_number]
+print(ClassRow)
+ClassString = ClassRow['Coarse Class Name (str)']
+
+print(ClassString)
